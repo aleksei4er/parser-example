e2464ac5c3b7e22a2b9f5a58fcee35f77e098539
Aleksei <aleks@alexey.chyorny.fvds.ru>
2021-02-12 16:39:30 +0300

    Change table column to content

--- src/Backend/Application/DataTransformers/ItemDTO.php
+++ src/Backend/Application/DataTransformers/ItemDTO.php
@@ -16,7 +16,7 @@ class ItemDTO
         return array_map(function(array $item) {
             $data = json_decode($item['data'], true);
             $item['h1'] = $data['h1'] ?? '';
-            $item['data'] = mb_substr(strip_tags($data['content'] ?? ''), 0, 200);
+            $item['content'] = mb_substr(strip_tags($data['content'] ?? ''), 0, 200);
             return $item;
         }, $items);
     }
